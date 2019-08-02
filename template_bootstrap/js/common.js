77aa87a055d158d91d5aff7531a32672eda3058e
Anatoly <jozmainmail@gmail.com>
2019-08-02 13:35:57 +0700

    Сверстаны остальные секции и подвал

--- template_bootstrap/js/common.js
+++ template_bootstrap/js/common.js
@@ -1,5 +1,22 @@
 $(document).ready(function() {
 
+	$(".mc_item_wrap ul").each(function() {
+		$(this).after("<div class='mc_item_wrap_af'></div>");
+	});
+	$(".mc_toggle").click(function() {
+		if($(this).parent().parent().children("ul").is(":visible")) {
+			$(this).parent().parent().children("ul").slideUp();
+			$(this).parent().parent().children(".mc_item_wrap_af").slideUp();
+		} else {
+			$("body .mc_wrap .mc_item_wrap > ul").hide();
+			$(".mc_item_wrap_af").hide();
+			$(".mc_item_wrap").removeClass("active");
+			$(this).parent().parent().addClass("active");
+			$(this).parent().parent().children("ul").slideDown();
+			$(this).parent().parent().children(".mc_item_wrap_af").slideDown();
+		}
+	});
+
 	//Цели для Яндекс.Метрики и Google Analytics
 	$(".count_element").on("click", (function() {
 		ga("send", "event", "goal", "goal");
